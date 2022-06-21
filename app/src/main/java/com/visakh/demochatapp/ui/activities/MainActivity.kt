a49c8d0e974516a11fcc21ac605a1524e1e44d01
VisakhC <visakh@meridian.net.in>
2022-06-21 10:33:21 +0530

    Base Commit

---
+++ app/src/main/java/com/visakh/demochatapp/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.visakh.demochatapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.visakh.demochatapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
